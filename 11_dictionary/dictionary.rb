479f20f491ed6e43a3cbd67acfbdca330c0071c7
nchen777 <nchen777@gmail.com>
2014-01-27 23:31:59 -0500

    updated

--- 11_dictionary/dictionary.rb
+++ 11_dictionary/dictionary.rb
@@ -1,9 +1,50 @@
 class Dictionary
-  attr_accessor :keywords, :entries
+  
   def initialize 
     @entries = {}
   end
   
-  def add (keywords, definition)
+  def entries
+    @entries
   end
+  
+  def add (word)
+    if word.class == Hash
+      key = nil
+      value = nil
+      word.each_key {|x| key = x}
+      word.each_value {|y| value = y}
+      @entries[key] = value
+    else word.class == String
+      @entries[word] = nil
+    end
+  end
+  
+  def include?(key)
+    @entries.each_key do |x|
+      return true if x == key
+    end
+    false
+  end
+  
+  def keywords
+    @entries.keys.sort
+  end
+  
+  def find (word)
+    result = {}
+    @entries.each_pair do |key, definition|
+      if key =~ /^#{word}/
+        result[key] = definition
+      end
+    end
+    result
+  end
+  
+  def printable
+    @entries.map do |word|
+      %Q{[#{word.first}] "#{word.last}"}
+    end.sort.join("\n")
+  end
+
 end
---
+++ 12_rpn_calculator/rpn_calculator.rb
@@ -0,0 +1,80 @@
+class RPNCalculator
+  attr_accessor :stack
+  
+  def initialize
+    @stack = []
+  end
+  
+  def value
+    return @stack[-1]
+    #use return to have it spit out the number that is in the array
+  end
+  
+  def push(number)
+    @stack.push(number)
+    #shoves number into array
+  end
+  
+  def pop
+    while true
+      x = @stack.pop
+      if x.nil?
+        raise "calculator is empty"
+      else
+        return x
+      end
+    end
+    #rips last number put into array out of array
+    #pop follows LIFO
+  end
+  
+  def plus
+    @stack.push(pop + pop)
+    #rips last two number shoved into array, then sums those two numbers, afterwards shoves the sum back into array
+    # that is why  pushing 2, 3, 4 the first plus gives you 7, take out 3 and 4 and sum = 7, then shove into array giving you 2 and 7, then second plus gives you 9 b/c you take out 2 and 7 and sum = 9
+  end
+  
+  def minus
+    last = pop
+    @stack.push(pop - last)
+    #want to get -1, so first pop the last number in(3), then pop the next last number, and then subtract the second last number from first last number 2-3
+  end
+  
+  def divide
+    last = pop
+    @stack.push(pop / last.to_f)
+  end
+  
+  def times
+    @stack.push(pop * pop)
+  end
+  
+  def tokens(input)
+    symbols = %w[+ - * /]
+    input.split.map do |things|
+      if symbols.include?(things)
+        things.to_sym
+      else things.to_f
+      end
+    end
+  end
+  
+  def evaluate (input)
+    tokens(input).each do |things|
+      if things == :+
+        plus
+      elsif things == :-
+        minus
+      elsif things == :*
+        times
+      elsif things == :/
+        divide
+      else 
+        push(things)
+      end
+    end
+    value
+  end
+  
+   
+end
